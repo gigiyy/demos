91c2c8e0ba7730655bd2183eb6432c8396411a05
Guixin Zhu <guixin.zhu@yahoo.com>
2022-12-10 12:20:42 +0900

    comment out assertions

--- src/main/java/com/example/demo/state/BaseState.java
+++ src/main/java/com/example/demo/state/BaseState.java
@@ -1,5 +1,7 @@
 package com.example.demo.state;
 
+import org.apache.logging.log4j.util.Strings;
+
 import java.util.HashMap;
 import java.util.Map;
 
@@ -7,6 +9,8 @@ public class BaseState {
     private static final ThreadLocal<Map<String, String>> state = ThreadLocal.withInitial(HashMap::new);
 
     public void setUuid(String uuid) {
+        // thread pool will keep the data from previous thread
+        // assert Strings.isEmpty(getUuid());
         BaseState.state.get().put("uuid", uuid);
     }
 
