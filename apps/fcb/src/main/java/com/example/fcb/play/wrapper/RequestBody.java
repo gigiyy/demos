ea6ec1dbaff8803a605a38a7c982188986232e4c
Guixin Zhu <guixinz@vmware.com>
2022-12-29 20:07:24 +0900

    suppress unchecked warning

--- apps/fcb/src/main/java/com/example/fcb/play/wrapper/RequestBody.java
+++ apps/fcb/src/main/java/com/example/fcb/play/wrapper/RequestBody.java
@@ -2,6 +2,7 @@ package com.example.fcb.play.wrapper;
 
 public interface RequestBody<T extends RequestBody<T>> {
 
+    @SuppressWarnings("unchecked")
     default RequestWrapper<T> wrap() {
         return new RequestWrapper<T>((T) this);
     }
--- apps/fcb/src/main/java/com/example/fcb/play/wrapper/ResponseBody.java
+++ apps/fcb/src/main/java/com/example/fcb/play/wrapper/ResponseBody.java
@@ -2,6 +2,7 @@ package com.example.fcb.play.wrapper;
 
 public interface ResponseBody<T extends ResponseBody<T>> {
 
+    @SuppressWarnings("unchecked")
     default ResponseWrapper<T> wrap() {
         return new ResponseWrapper<>((T) this);
     }
