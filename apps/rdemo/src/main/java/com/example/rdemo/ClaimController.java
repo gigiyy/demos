d86f2bd37e655630268dbae9e553792eb74e8fb4
Guixin Zhu <guixinz@vmware.com>
2023-01-16 23:29:06 +0900

    change forEach to toMap collector

--- apps/rdemo/src/main/java/com/example/rdemo/ClaimController.java
+++ apps/rdemo/src/main/java/com/example/rdemo/ClaimController.java
@@ -2,15 +2,15 @@ package com.example.rdemo;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.FieldError;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.support.WebExchangeBindException;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import javax.validation.Valid;
-import java.util.HashMap;
 import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/claims")
@@ -37,12 +37,16 @@ public class ClaimController {
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(WebExchangeBindException.class)
     Mono<Map<String, String>> handleValidationException(WebExchangeBindException ex) {
-        Map<String, String> errors = new HashMap<>();
-        ex.getBindingResult().getFieldErrors().forEach(error -> {
-            String field = error.getField();
-            String message = error.getDefaultMessage();
-            errors.put(field, message);
-        });
+        Map<String, String> errors = ex.getBindingResult().getFieldErrors()
+                .stream()
+                .collect(Collectors.toMap(FieldError::getField, getDefaultMessage()));
         return Mono.just(errors);
+    }
+
+    private Function<FieldError, String> getDefaultMessage() {
+        return error -> {
+            String msg = error.getDefaultMessage();
+            return msg == null ? "error message not set" : msg;
+        };
     }
 }
