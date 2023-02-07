d0a7809d4568a242f16ac48e337977f00a043c09
Guixin Zhu <guixinz@vmware.com>
2023-02-07 16:12:37 +0900

    add seqNo field for claim entitiy

--- apps/fcb/src/main/java/com/example/fcb/claim/service/ClaimEntity.java
+++ apps/fcb/src/main/java/com/example/fcb/claim/service/ClaimEntity.java
@@ -3,6 +3,8 @@ package com.example.fcb.claim.service;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -16,6 +18,8 @@ public class Claim {
     @Id
     @GeneratedValue
     private Long id;
+    @JsonProperty("seqNo")
+    private String sequenceNumber;
     private String sender;
     private String receiver;
     private String message;
