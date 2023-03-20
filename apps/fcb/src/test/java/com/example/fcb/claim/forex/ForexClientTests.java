f1786e3a0fe1e1d94a1539bf32ce849cdc49c6c3
Guixin Zhu <guixinz@vmware.com>
2023-03-20 21:53:32 +0900

    use copied mappings for stubs

--- apps/fcb/src/test/java/com/example/fcb/claim/forex/ForexClientTests.java
+++ apps/fcb/src/test/java/com/example/fcb/claim/forex/ForexClientTests.java
@@ -1,8 +1,5 @@
 package com.example.fcb.claim.forex;
 
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-
-import java.math.BigDecimal;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,11 +7,17 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.cloud.contract.stubrunner.spring.AutoConfigureStubRunner;
 import org.springframework.cloud.contract.stubrunner.spring.StubRunnerProperties.StubsMode;
+import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.math.BigDecimal;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(webEnvironment = WebEnvironment.NONE)
-@AutoConfigureStubRunner(ids = "com.example:forex:+:stubs:8082", stubsMode = StubsMode.LOCAL)
+//@AutoConfigureStubRunner(ids = "com.example:forex:+:stubs:8082", stubsMode = StubsMode.LOCAL)
+@AutoConfigureWireMock(port = 8082)
 public class ForexClientTests {
 
     @Autowired
